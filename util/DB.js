c08a1dd4d9f2c7400b8756d11d058879ad02a790
Tom Dunlap <tdunlap@pivotal.io>
2018-07-07 21:12:49 -0700

    pretty print the db on write
    so that it's easier to read in the git diffs
    
    Signed-off-by: Katie Conforti <k80@katieconforti.com>

--- util/DB.js
+++ util/DB.js
@@ -4,7 +4,7 @@ const dbPath = '../db.json';
 const DB = {
   read: () => require(dbPath),
   write: (newDb) => {
-    const serializedNewDb = JSON.stringify(newDb) + '\n';
+    const serializedNewDb = JSON.stringify(newDb, null, 2) + '\n';
     fs.writeFileSync(`${__dirname}/${dbPath}`, serializedNewDb);
   }
 };
